1c0bdc688f8d1003f54bec137889f85477258eb7
AdithyaKrishna169 <adithyakrish169@gmail.com>
2022-01-12 12:07:33 +0530

    final

--- pro/app1/models.py
+++ pro/app1/models.py
@@ -4,8 +4,15 @@ from datetime import datetime
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Message(models.Model):
     value = models.CharField(max_length=1000000)
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)
+
+    def __str__(self) -> str:
+        return self.user
